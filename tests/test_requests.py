2b9c2bbc5791502b934dc68743d10242e56d5eb1
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-07 17:16:59 +0200

    adding validate_move flask tests of response codes

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -25,6 +25,24 @@ class Flask_tests(unittest.TestCase):
         status_code = response.status_code
         self.assertEqual(409, status_code)
 
+    def test_flask_endpoint_validate_move_code_200(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/v1/rook/h1/h2")
+        status_code = response.status_code
+        self.assertEqual(200, status_code)
+
+    def test_flask_endpoint_validate_move_code_404(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/v1/rookie/h1")
+        status_code = response.status_code
+        self.assertEqual(404, status_code)
+
+    def test_flask_endpoint_list_validate_move_code_409(self):
+        tester = app.test_client(self)
+        response = tester.get("/api/v1/rook/h1/g")
+        status_code = response.status_code
+        self.assertEqual(409, status_code)
+
 
 if __name__ == "__main__":
     unittest.main()
