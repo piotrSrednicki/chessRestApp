f108dd32eecb5c0b72fa5ca94fc7e0f2f4da3dfb
piotrsrednickiDS360 <piotrsrednicki@ds360.pl>
2023-04-07 10:51:21 +0200

    implementing all figures

--- models/BishopFigure.py
+++ models/BishopFigure.py
@@ -1,14 +1,38 @@
-from typing import List
-
 from models.AbstractFigure import Figure
+from models.Chessboard import Chessboard
 
 
 class BishopFigure(Figure):
-    def __init__(self, field):
+    def __init__(self, field: tuple[int, int]):
         super().__init__(field)
+        self.field = field
+        # top-left, top, top-right...
+        # x - abcd
+        # y - 1234
+        self.moves_list: list[(int, int)] = [(x, y) for x, y in
+                                             zip(range(-7, 8), range(-7, 8))
+                                             if x != 0 and y != 0] \
+                                            + [(x, y) for x, y in
+                                               zip(range(-7, 8), reversed(range(-7, 8)))
+                                               if x != 0 and y != 0]
+        self.min_x_and_y: int = 0
+        self.max_x_and_y: int = 7
+        self.chessboard: Chessboard = Chessboard()
 
-    def list_available_moves(self) -> List[str]:
-        pass
+    def list_available_moves(self) -> list[str]:
+        assert self.field[0] in range(self.min_x_and_y, self.max_x_and_y + 1) \
+               and self.field[1] in range(self.min_x_and_y, self.max_x_and_y + 1)
+        available_moves: list[str] = []
+        for move in self.moves_list:
+            position_after_move: tuple[int, int] = self.field[0] + move[0], self.field[1] + move[1]
+            if position_after_move[0] in range(self.min_x_and_y, self.max_x_and_y + 1) \
+                    and position_after_move[1] in range(self.min_x_and_y, self.max_x_and_y + 1):
+                available_moves.append(self.chessboard.int_Touple_To_Chess_PositionStr(position_after_move))
+        return available_moves
 
-    def validate_move(self, dest_field: tuple) -> tuple[str, str]:
-        pass
+    def validate_move(self, dest_field: tuple) -> str:
+        possible_moves = self.list_available_moves()
+        print(dest_field, possible_moves)
+        if self.chessboard.int_Touple_To_Chess_PositionStr(dest_field) in possible_moves:
+            return "valid"
+        return "invalid"
